3cce85cc9568496ce5dc54e756538a3b5bb2f697
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-11 15:19:11 +0100

    starting to expand the rules. still can't exit case at right point

--- matches.rb
+++ matches.rb
@@ -1,7 +1,7 @@
 require 'pry-byebug'
 
 def hand_pair
-  puts case
+  case
     when
       @hand[0][0].include?(@hand[1][0])
       return true
@@ -10,7 +10,7 @@ end
 
 
 def flop_pair
-  puts case
+  case
     when
       @flop[0][0].include?(@flop[1][0])
       return true
@@ -18,8 +18,32 @@ def flop_pair
 end
 
 
+def hand_matches_index_0
+  case
+    when
+      @hand[0][0].include?(@flop[0][0])
+      return true
+    when
+      @hand[1][0].include?(@flop[0][0])
+      return true
+  end
+end
+
+
+def hand_matches_index_1
+  case
+    when
+      @hand[0][0].include?(@flop[1][0])
+      return true
+    when
+      @hand[1][0].include?(@flop[1][0])
+      return true
+  end
+end
+
+
 def flop_matches_index_0
-  puts case
+  case
     when
       @flop[0][0].include?(@hand[0][0])
       return true
@@ -31,7 +55,7 @@ end
 
 
 def flop_matches_index_1    
-  puts case
+  case
     when
       @flop[0][0].include?(@hand[1][0])
       return true  
@@ -42,53 +66,64 @@ def flop_matches_index_1
 end
 
 def state_pair
-  puts case
-  when
-    hand_pair == true
-    puts "pair"
-  when
-    flop_pair == true
-    puts "pair"
-  when
-    flop_matches_index_0 == true
-    puts "pair"
-  when
-    flop_matches_index_1 == true
-    puts "pair"
+  case
+    when
+      hand_pair == true
+      puts "pair"
+    when
+      flop_pair == true
+      puts "pair"
+    when
+      flop_matches_index_0 == true
+      puts "pair"
+    when
+      flop_matches_index_1 == true
+      puts "pair"
   end
 end
 
 def threes
-  puts case
+  case
     when
       hand_pair && flop_matches_index_0 == true
       puts "three"
     when
       hand_pair && flop_matches_index_1 == true
-      puts "three"      
+      puts "three" 
+    when
+      flop_pair && hand_matches_index_0 == true
+      puts "three"
+    when
+      flop_pair && hand_matches_index_1 == true
+      puts "three"
   end
 end 
 
 
 def two_pair
-  puts case
+  case
     when
       hand_pair && flop_pair == true
       puts "two pairs"
+    when
+      flop_matches_index_1 && flop_matches_index_0 == true
+      puts "two pairs"
   end
 end
- 
+
+
 def rules
   puts case
   when
-    threes == true
+    threes 
     "three"
   when
-    two_pair == true
+    two_pair 
     "two pairs"
   when
-    state_pair == true
+    state_pair 
     "pair"
+  binding.pry 
   end
 end
 
--- simple_cards.rb
+++ simple_cards.rb
@@ -1,11 +1,11 @@
 require './matches.rb'
-
+require 'pry-byebug'
 
 suits = ["#", "@", "!", "^"]
-numbers = ["1", "2", "3"]
+numbers = ["A", "2", "3", "4", "5","6", "7", "8", "9", "10", "J", "Q", "k"]
 @pack = numbers.product(suits)
 @pack.shuffle!
-
+@pot = 0
 
 
 def hand(cards = 2)
@@ -14,21 +14,34 @@ end
 
 @hand = hand.sort!
 
-def flop (cards = 2)
+def flop (cards = 3)
   @pack.pop(cards)
 end
 
 @flop = flop.sort!
 
+def turn
+  @pack.pop
+end
 
+@turn = turn
 
+def river
+  @pack.pop
+end
 
+@river = river
+
+def place_bets
+  puts "Please enter bets: "
+  answer = gets.chomp.to_i
+  @pot += answer
+  puts "The pot is #{@pot}"
+end
 
 
 
 system("clear")
-print @pack.join(" ")
-puts
 print @hand.join(" ")
 puts
 print @flop.join(" ")
